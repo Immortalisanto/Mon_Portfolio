d7144a7ae82f35697e274d3de4c5de19f3cacaa1
Immortalisanto <tur.anthony91@gmail.com>
2023-07-04 12:52:44 +0200

    Intégration footer OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Header from './sections/Header/Header'
+import Footer from './sections/Footer/Footer'
+import React from 'react'
 
 function App() {
-    return <Header />
+    return (
+        <React.Fragment>
+            <Header />
+            <Footer />
+        </React.Fragment>
+    )
 }
 
 export default App
---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import logoLinkedIn from '../../assets/images/linkedIn.png'
+import logoGithub from '../../assets/images/github.png'
+import './Footer-style.scss'
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer__logo">
+                <a href="#">
+                    <img
+                        src={logoLinkedIn}
+                        alt="Logo LinkedIn, possibilité de cliquer pour être redirigé vers le profil LinkedIn d'Anthony Tur"
+                        className="footer__logo--linkedIn logo"
+                    />
+                </a>
+                <a href="#">
+                    <img
+                        src={logoGithub}
+                        alt="Logo Github, possibilité de cliquer pour être redirigé vers le profil github d'Anthony Tur"
+                        className="footer_logo-github logo"
+                    />
+                </a>
+            </div>
+            <p className="footer__copyright">Copyright © 2023 - Anthony Tur</p>
+            <div className="footer__twoLinksForLegalInformations">
+                <p className="footer__legalInformations--privacyPolicy legalInformation">
+                    <a
+                        href="#"
+                        className="linksLegalInformations">
+                        Politique de confidentialité
+                    </a>
+                </p>
+                <p className="footer__legalInformations--legalNotice legalInformation">
+                    <a
+                        href="#"
+                        className="linksLegalInformations">
+                        Mentions légales
+                    </a>
+                </p>
+            </div>
+        </footer>
+    )
+}
