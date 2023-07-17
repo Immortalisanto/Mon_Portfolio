e47246e9f1844f36b40652df4604ea3f941cfc1f
Immortalisanto <tur.anthony91@gmail.com>
2023-07-17 16:39:02 +0200

    Ajout de tous les projets pour la section 'Mes réalisations'

--- src/data/RealisationsData.js
+++ src/data/RealisationsData.js
@@ -1,33 +1,51 @@
 import bookiCarousel from '../assets/worksImages/desktop/carousel/booki.jpg'
 import sophieBluelCarousel from '../assets/worksImages/desktop/carousel/sophie-bluel.jpg'
+import ninaCarducciCarousel from '../assets/worksImages/desktop/carousel/nina-carducci.jpg'
+import kasaCarousel from '../assets/worksImages/desktop/carousel/kasa.jpg'
+import monVieuxGrimoireCarousel from '../assets/worksImages/desktop/carousel/mon-vieux-grimoire.jpg'
+import monPortfolioCarousel from '../assets/worksImages/desktop/carousel/mon-portfolio.jpg'
 
 export const RealisationsData = [
     {
         index: 0,
         image: bookiCarousel,
-        title: 'Work 1',
+        title: 'Booki',
         description: 'Intégration blablabla et blablabla',
-        tags: ['HTML', 'CSS', 'Javascript'],
+        tags: ['HTML', 'CSS'],
     },
     {
         index: 1,
         image: sophieBluelCarousel,
-        title: 'Work 2',
+        title: 'Sophie Bluel',
         description: 'Intégration blablabla et blablabla',
-        tags: ['HTML', 'CSS', 'Javascript'],
+        tags: ['Javascript'],
     },
     {
         index: 2,
-        image: '',
-        title: 'Work 3',
+        image: ninaCarducciCarousel,
+        title: 'Nina Carducci',
         description: 'Intégration blablabla et blablabla',
-        tags: ['HTML', 'CSS', 'Javascript'],
+        tags: ['Débug', 'Optimisation'],
     },
     {
         index: 3,
-        image: '',
-        title: 'Work 4',
+        image: kasaCarousel,
+        title: 'Kasa',
         description: 'Intégration blablabla et blablabla',
-        tags: ['HTML', 'CSS', 'Javascript'],
+        tags: ['HTML', 'CSS', 'React'],
+    },
+    {
+        index: 4,
+        image: monVieuxGrimoireCarousel,
+        title: 'Mon vieux grimoire',
+        description: 'Intégration blablabla et blablabla',
+        tags: ['Node.js', 'Express', 'MongoDB'],
+    },
+    {
+        index: 5,
+        image: monPortfolioCarousel,
+        title: 'Mon portfolio',
+        description: 'Intégration blablabla et blablabla',
+        tags: ['HTML', 'Sass', 'React', 'Node.js', 'Express', 'MongoDB'],
     },
 ]
