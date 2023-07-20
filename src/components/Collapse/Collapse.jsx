d71b837b50815b13fb43bc53f59e7ac2d3c22600
Immortalisanto <tur.anthony91@gmail.com>
2023-07-20 17:30:38 +0200

    Avancement dans les collapses : Button OK, fermeture ouverture OK (manque animation), remplissage progressBar OK. Reste finitions

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -3,30 +3,32 @@ import arrow from '../../assets/images/arrow.png'
 import { useState } from 'react'
 
 export default function Collapse({ categorie, skills }) {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(true)
 
     return (
         <div className="collapse">
             <button
-                className="button"
+                className={`collapse__button ${isOpen ? '' : 'buttonRadius'}`}
                 onClick={() => setIsOpen(!isOpen)}>
-                <h3 className="button__title">{categorie}</h3>
+                <h3 className="collapse__button__title">{categorie}</h3>
                 <img
-                    className="button__arrow"
+                    className={`collapse__button__arrow--${
+                        isOpen ? 'top' : 'down'
+                    }`}
                     src={arrow}
                     alt="Flèche permettant l'ouverture et la fermeture de l'ensemble de compétences"
                 />
             </button>
-            <div className={`skillBox ${isOpen ? '' : 'displayNone'}`}>
+            <div className={`skillBox--${isOpen ? 'open' : 'closed'}`}>
                 {skills.map(({ skill, level, index }) => (
                     <div
                         key={`${skill}-${index}`}
-                        className="skillBox_skill">
-                        <h4 className="skill__title">{skill}</h4>
-                        <div className="skill__progressBar">
+                        className="skillBox__skill">
+                        <h4 className="skillBox__skill__title">{skill}</h4>
+                        <div className="skillBox__skill__progressBar">
                             <div
-                                className="skill__progressBar--level"
-                                width={level}></div>
+                                className="skillBox__skill__progressBar--level"
+                                style={{ width: `${level}` }}></div>
                         </div>
                     </div>
                 ))}
